f22e601773dd6786f8fe25f0e7f2fb2bb3e3f6fb
Evgenii Nikitin <evgeny.nikitin1991@gmail.com>
2018-05-10 17:59:43 +0300

    init

---
+++ README.md
@@ -0,0 +1,46 @@
+Url column
+==========
+Column for creating links for the grid
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist en-develop/yii2-grid-link-column "*"
+```
+
+or add
+
+```
+"en-develop/yii2-grid-link-column": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?=  
+     yii\grid\GridView::widget([
+         //...
+         [
+             'class' => 'edevelop\grid\LinkColumn',
+             'attribute' => 'yourAttribute',
+             'url' => 'your url',
+             'params' => [
+                'urlParam' => 'modelAttribute'
+             ],
+             'linkOptions' => []
+         ]
+         //...
+     ]);
+?>
+```
---
+++ UrlColumn.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace edevelop\grid;
+
+use yii\grid\DataColumn;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+/**
+ * Class LinkColumn
+ * To add an UrlColumn to the gridview, add it to the [[GridView::columns|columns]] configuration as follows:
+ *   ...
+ *   [
+ *       'class' => 'edevelop\grid\LinkColumn',
+ *       'attribute' => 'yourAttribute',
+ *       'url' => 'your url',
+ *       'params' => [
+ *           'urlParam' => 'modelAttribute'
+ *       ],
+ *       'urlOptions' => []
+ *   ]
+ *  ...
+ *
+ * @package edevelop\grid
+ */
+class LinkColumn extends DataColumn
+{
+    /**
+     * link
+     * @var
+     */
+    public $url = '/';
+
+    /**
+     * Params for link
+     * @var
+     */
+    public $params = [];
+
+    /**
+     * Options for link settings
+     * @var
+     */
+    public $urlOptions = [];
+
+    /**
+     * @var string
+     */
+    public $format = 'raw';
+
+    /**
+     * {@inheritdoc}
+     * @throws \yii\base\InvalidArgumentException
+     */
+    protected function renderDataCellContent($model, $key, $index)
+    {
+        return Html::a(
+            $model->{$this->attribute},
+            $this->generateUrl($model),
+            $this->urlOptions
+        );
+    }
+
+
+    /**
+     * @param $model
+     * @throws \yii\base\InvalidArgumentException
+     * @return string
+     */
+    protected function generateUrl($model)
+    {
+        $url[] = $this->url;
+
+        if (null !== $this->params && is_array($this->params)) {
+            foreach ($this->params as $key => $param) {
+                $url[$key] = $model->$param;
+            }
+        }
+
+        return Url::to($url);
+    }
+}
